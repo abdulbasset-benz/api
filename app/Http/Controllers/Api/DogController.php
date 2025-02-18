75a5929211d6c06adc646bcc7518387cf793f1a8
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-18 13:27:23 +0100

    Apply request class and modify controller store and update methods

--- app/Http/Controllers/Api/DogController.php
+++ app/Http/Controllers/Api/DogController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use App\Models\Dog;
 use Illuminate\Http\Request;
+use App\Http\Requests\DogRequest;
+use App\Http\Controllers\Controller;
 
 class DogController extends Controller
 {
@@ -19,9 +20,9 @@ class DogController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(DogRequest $request)
     {
-        return Dog::create($request->only(['name', 'breed']));
+        return Dog::create($request->validated());
     }
 
     /**
@@ -35,10 +36,10 @@ class DogController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(DogRequest $request, string $id)
     {
         $dog = Dog::findOrFail($id);
-        $dog->update($request->only(['name', 'breed']));
+        $dog->update($request->validated());
         return $dog;
     }
 
