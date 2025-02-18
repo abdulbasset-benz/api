d1ce6ba6843b03b29848892294fc605f83cdb510
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-18 13:45:45 +0100

    add authentication routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DogController;
@@ -11,3 +12,7 @@ Route::get('/user', function (Request $request) {
 
 
 Route::apiResource('/dogs', DogController::class);
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/logout', [AuthController::class, 'logout']);
