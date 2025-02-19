ba8efdad19ce8a485cdc648d28ad9289d22b7818
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-19 21:18:04 +0100

    add sanctum middelware for route

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ Route::apiResource('/dogs', DogController::class);
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
