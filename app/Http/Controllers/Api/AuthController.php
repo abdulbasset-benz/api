6dd880c32ff9979a798f82cf94f4bc0634778cdb
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-02-19 21:16:42 +0100

    add logout method and test it

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -50,4 +50,11 @@ class AuthController extends Controller
         ], 200);
     }
 
+    public function logout(Request $request){
+        $request->user()->tokens()->delete();
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ], 200);
+    }
+
 }
